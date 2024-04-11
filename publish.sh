f350b83c8aafc78a2bd8134358650a0addc1d020
Ronnie B <ronniedockab@gmail.com>
2024-04-11 19:15:45 -0400

    Add fix and update gitignore

--- publish.sh
+++ publish.sh
@@ -1,7 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 . ~/.nvm/nvm.sh
 
+loc="loc: publish.sh;"
+echo "${loc} Enter."
+
+if [ -z "$VA_NEXUS_USER" ]; then
+    echo "${loc} Warning; VA_NEXUS_USER not set"
+    exit 1
+fi
+
+if [ -z "$VA_NEXUS_PWD" ]; then
+    echo "${loc} Warning; VA_NEXUS_PWD not set"
+    exit 1
+fi
+
+echo "${loc} Initializing .npmrc"
 NEXUS=$VA_NEXUS_USER:$VA_NEXUS_PWD
 AUTH=`echo -n $NEXUS | openssl base64`
 
@@ -11,6 +25,7 @@ email=nobody
 always-auth=true
 strict-ssl=false" > .npmrc
 
+echo "${loc} Exit."
 nvm install v20.12.1
 npm pack
 npm publish --verbose
